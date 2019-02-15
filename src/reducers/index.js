23c686b89d7d6f18405ba7b35099d375326b5ffa
mirandaklein <mirandaklein@mac.com>
2019-02-15 15:44:49 -0700

    added actions

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import LibraryReducer from './LibraryReducer.js';
+import SelectionReducer from './SelectionReducer.js';
 
 export default combineReducers({
-    libraries: LibraryReducer
+    libraries: LibraryReducer,
+    selectedLibraryId: SelectionReducer
 });
