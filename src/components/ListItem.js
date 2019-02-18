082e1d45f92fd663b6c826eace6e4c075f081ae8
mirandaklein <mirandaklein@mac.com>
2019-02-18 12:34:33 -0700

    actions connected

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,26 +1,42 @@
 import React, { Component } from 'react';
-import { View,Text } from 'react-native';
+import { View, Text, TouchableWithoutFeedback, LayoutAnimation } from 'react-native';
 import { connect  } from 'react-redux';
 import CardItem from './CardItem.js';
 import * as actions from '../actions';
 
 class ListItem extends Component {
+    componentWillUpdate() {
+        LayoutAnimation.spring();
+    };
+
+    renderDescription() {
+        const { library, expanded } = this.props;
+
+            if (expanded) {
+                return (
+                    <CardItem>
+                        <Text>{library.description}</Text>
+                    </CardItem>
+                );
+            }
+        }
     render() {  
         const { titleStyle } = styles;
+        const { id, title } = this.props.library;
 
         return(
-            <View>
-                <CardItem>
-                    <Text style={titleStyle}>
-                    {this.props.library.title}
-                    </Text>
-                </CardItem>
-                <CardItem>
-                    <Text style={titleStyle}>
-                    {this.props.library.description}
-                    </Text>
-                </CardItem>
-            </View>
+            <TouchableWithoutFeedback
+                onPress={() => this.props.selectLibrary(id)}
+            >
+                <View>
+                    <CardItem>
+                        <Text style={titleStyle}>
+                        {title}
+                        </Text>
+                    </CardItem>
+                    {this.renderDescription()}
+                </View>
+            </TouchableWithoutFeedback>
         )
     }
 }
@@ -32,4 +48,9 @@ const styles= {
     }
 }
 
-export default connect(null, actions)(ListItem);
+const mapStateToProps = (state, ownProps) => {
+    const expanded = state.selectedLibraryId === ownProps.library.id;
+    return { expanded };
+}
+
+export default connect(mapStateToProps, actions)(ListItem);
